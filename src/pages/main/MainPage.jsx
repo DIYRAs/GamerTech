e3e8e4ff944cc44e764bc8a36fe9e7267d9e168d
DIYRA <diyra418@gmail.com>
2025-07-17 16:39:59 +0800

    Update MainPage.jsx

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from './sections/Hero'
-import Items from './sections/items'
+import Items from './sections/Items'
 import Featured from './sections/Featured'
 import Footer from './sections/Footer'
 import Navbar from '../global/Navbar'
